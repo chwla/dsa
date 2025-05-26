344e63b60d8b72810a0ea87bb15dbbd92d39778d
chwla <chwla75@gmail.com>
2025-05-26 18:52:56 +0530

    optimised 169 array

--- arrays/169.cpp
+++ arrays/169.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int num = -1, count=0;
         int n=nums.size();
-        sort(nums.begin() , nums.end());
-
-        int count=1, maxcount=1;
-        int maxelement = nums[0];
-        for(int i=1;i<n;i++){
-
-            if(nums[i]==nums[i-1])count++;
-            else count=1;
-            if(maxcount<count){
-                maxcount=count;
-                   maxelement=nums[i];
-                }
-            if(maxcount>n/2){
-                return maxelement;
+        for(int i=0;i<n;i++){
+            if(nums[i]==num){
+                count++;
             }
-
+            else if(count==0)num = nums[i];
+            else count--;
         }
-        return maxelement;
+        count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==num)count++;
+        }
+        if(count>(n/2))return num;
+        return num;
     }
 };
