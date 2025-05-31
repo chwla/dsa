8446fde4e9fddc5c67826279cf7d637a472f8f94
chwla <chwla75@gmail.com>
2025-05-31 14:16:39 +0530

    328 ll

---
+++ Linked List/328.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even; // Save starting point of even list
+
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead; // Append even list to end of odd list
+        return head;
+    }
+};
